3c7c5c4b95f3ffc5be60dab213f1cfb16c4a5b1b
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-09-01 15:52:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-#remove everthing and add you run cmds and use this if builder os set in koyebbash run.sh
+#remove everthing and add you run cmds and use this if builder os set in koyeb use (bash run.sh)
 python3 main.py &
 python3 app.py &
 wait
